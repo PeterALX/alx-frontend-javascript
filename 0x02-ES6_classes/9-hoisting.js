2701eedd26b2a7d2536a9740a6933d06ad088182
PeterALX <quaypeter25@gmail.com>
2024-10-06 15:23:36 +0300

    another attempt at 9

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -12,7 +12,7 @@ class HolbertonClass {
     return this._location;
   }
 }
-export class StudentHolberton {
+class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
