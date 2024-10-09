7e8f62ec048ed2dd1732830db3126aea6ca97928
PeterALX <quaypeter25@gmail.com>
2024-10-09 19:36:16 +0300

    done 100

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+    return;
+  }
+  const count = weakMap.get(endpoint);
+  if (count >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+  weakMap.set(endpoint, count + 1);
+}
+export { weakMap, queryAPI };
